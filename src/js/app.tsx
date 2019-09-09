36db27970ab38febe8c36a55b37dec14d59d49bb
Ruslan Korkin <rukrak86@gmail.com>
2019-09-09 17:23:22 +0300

    add Search button;

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -1,14 +1,52 @@
 import React, { Fragment } from 'react';
 import Button from '@material-ui/core/Button';
-import Logo from './logo';
+
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+//import Logo from './logo';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      maxWidth: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  }),
+);
 
 function App() {
+  const classes = useStyles();
   return (
     <Fragment>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-      <Logo />
+      <Paper className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder="Enter plate number or VIN number"
+          inputProps={{ 'aria-label': 'search google maps' }}
+        />
+        <IconButton className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </Fragment>
 
   );
