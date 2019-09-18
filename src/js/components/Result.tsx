b2f2b3c068c621649e28faf39b852fb9f86e3819
Ruslan Korkin <rukrak86@gmail.com>
2019-09-18 23:13:10 +0300

    describe Results;

--- src/js/components/Result.tsx
+++ src/js/components/Result.tsx
@@ -22,12 +22,32 @@ import ImageIcon from '@material-ui/icons/Image';
 import WorkIcon from '@material-ui/icons/Work';
 import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
+import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import LinearScaleIcon from '@material-ui/icons/LinearScale';
+import ColorLensIcon from '@material-ui/icons/ColorLens';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import LocalGasStationIcon from '@material-ui/icons/LocalGasStation';
+import MergeTypeIcon from '@material-ui/icons/MergeType';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import FormatListNumberedRtlIcon from '@material-ui/icons/FormatListNumberedRtl';
+import FingerprintIcon from '@material-ui/icons/Fingerprint';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import CopyrightIcon from '@material-ui/icons/Copyright';
+import ExploreIcon from '@material-ui/icons/Explore';
+import LineWeightIcon from '@material-ui/icons/LineWeight';
+import HomeIcon from '@material-ui/icons/Home';
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       //width: '100%',
-      //maxWidth: 360,
+      maxWidth: 500,
       //backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(0),
+      margin: theme.spacing(0),
     },
   }),
 );
@@ -40,11 +60,16 @@ export const Result = () => {
 
     return(
         <Fragment>
+            loaded: {JSON.stringify(state.itemIsLoaded)}
+            <Paper>
+                {state.itemResponse.n_reg_new}
+            </Paper>
+            <Paper>
             <List className={classes.root}>
                 <ListItem>
                     <ListItemAvatar>
                     <Avatar>
-                        <ImageIcon />
+                        <LocalShippingIcon />
                     </Avatar>
                     </ListItemAvatar>
                     <ListItemText primary="Body" secondary={state.itemResponse.body} />
@@ -52,21 +77,134 @@ export const Result = () => {
                 <ListItem>
                     <ListItemAvatar>
                     <Avatar>
-                        <WorkIcon />
+                        <DirectionsCarIcon />
                     </Avatar>
                     </ListItemAvatar>
-                    <ListItemText primary="Brand" secondary={state.itemResponse.brand} />
+                    <ListItemText primary="Brand" secondary={`${state.itemResponse.brand} ${state.itemResponse.model}`} />
                 </ListItem>
                 <ListItem>
                     <ListItemAvatar>
                     <Avatar>
-                        <BeachAccessIcon />
+                        <LinearScaleIcon />
                     </Avatar>
                     </ListItemAvatar>
-                    <ListItemText primary="Model" secondary={state.itemResponse.model} />
+                    <ListItemText primary="capacity" secondary={state.itemResponse.capacity} />
                 </ListItem>
-                </List> 
-            loaded: {JSON.stringify(state.itemIsLoaded)}
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <ColorLensIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="color" secondary={state.itemResponse.color} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <EventNoteIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="d_reg" secondary={state.itemResponse.d_reg} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <LocationOnIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="dep" secondary={state.itemResponse.dep} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <FormatListNumberedIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="dep_code" secondary={state.itemResponse.dep_code} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <LocalGasStationIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="fuel" secondary={state.itemResponse.fuel} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <MergeTypeIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="kind" secondary={state.itemResponse.kind} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <DateRangeIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="make_year" secondary={state.itemResponse.make_year} />
+                </ListItem>
+
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <FormatListNumberedRtlIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="oper_code" secondary={state.itemResponse.oper_code} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <FingerprintIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="oper_name" secondary={state.itemResponse.oper_name} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <FitnessCenterIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="own_weight" secondary={state.itemResponse.own_weight} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <CopyrightIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="person" secondary={state.itemResponse.person} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <ExploreIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="purpose" secondary={state.itemResponse.purpose} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <HomeIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="reg_addr_koatuu" secondary={state.itemResponse.reg_addr_koatuu} />
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <LineWeightIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="total_weight" secondary={state.itemResponse.total_weight} />
+                </ListItem>
+            </List> 
+            </Paper>
         </Fragment>
     )
 }
