4e4ae51770b21263ef7ba1595880c9e864954682
Ruslan Korkin <rukrak86@gmail.com>
2019-11-07 11:26:07 +0200

    add docs folder;

---
+++ docs/precache-manifest.9cc1b435845c676467a0a5884b5f22d9.js
@@ -0,0 +1,35 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e098a322e4fbf2aa2073cc6c6d839a",
+    "url": "audio/sharp.m4r"
+  },
+  {
+    "revision": "5c312ccf2a724a206b062be0acda9a5a",
+    "url": "audio/sharp.mp3"
+  },
+  {
+    "revision": "9157ccb685a92f05435701602cf66236",
+    "url": "audio/sharp.ogg"
+  },
+  {
+    "revision": "22c73f3189b66b894371e7b7cb8e8f63",
+    "url": "index.html"
+  },
+  {
+    "url": "js/main.fb2ebae354cbbcccb9d5.js"
+  },
+  {
+    "url": "js/runtime.fb2ebae354cbbcccb9d5.js"
+  },
+  {
+    "url": "js/vendors~main.fb2ebae354cbbcccb9d5.js"
+  },
+  {
+    "revision": "eb6aabe54d21a9d796f1e5f0ff896d33",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "388ed88eec82ddeacbf877ee7dc4b225",
+    "url": "robots.txt"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.9cc1b435845c676467a0a5884b5f22d9.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg|webp)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 20, purgeOnQuotaError: false })] }), 'GET');
+
+workbox.googleAnalytics.initialize({});
