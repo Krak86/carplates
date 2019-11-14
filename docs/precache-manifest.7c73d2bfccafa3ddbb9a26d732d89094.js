cadc5347c2f833edfe52963f1ee31c8f92ec2764
Ruslan Korkin <rukrak86@gmail.com>
2019-11-14 14:28:19 +0200

    update docs;

---
+++ docs/precache-manifest.7c73d2bfccafa3ddbb9a26d732d89094.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c312ccf2a724a206b062be0acda9a5a",
+    "url": "audio/sharp.mp3"
+  },
+  {
+    "revision": "e31adf066a966b01d090fab9768a9d5a",
+    "url": "index.html"
+  },
+  {
+    "url": "js/main.4116e15a174ee9e250c9.js"
+  },
+  {
+    "url": "js/runtime.4116e15a174ee9e250c9.js"
+  },
+  {
+    "url": "js/vendors~main.4116e15a174ee9e250c9.js"
+  },
+  {
+    "revision": "c7d675a6ee5ff4d4681b5ebed7e345de",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "388ed88eec82ddeacbf877ee7dc4b225",
+    "url": "robots.txt"
+  }
+]);
