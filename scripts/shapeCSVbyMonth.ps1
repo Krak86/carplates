127e457f920a222e3fbe6f514c96a45537ec57b8
Ruslan Korkin <rukrak86@gmail.com>
2019-11-18 23:19:57 +0200

    add powershel lscript to exclude data by month from CSV;

---
+++ scripts/shapeCSVbyMonth.ps1
@@ -0,0 +1,30 @@
+#manual step: download zip file from https://data.gov.ua/dataset/06779371-308f-42d7-895e-5a39833375f0
+#manual step: unzip it
+#manual step: put the path and file name
+#automatical step: add 2 new fields "PartitionKey" and "RowKey"
+#automatical step: export separate csv file per each 100000 items (it's easier to import data in azure storage explorer to azure table)
+
+#path to unziped csv file
+cd "C:\Users\rkor\Downloads\cosmosDB";
+#sourceCSVfileName, example: "tz_opendata_z01012019_po01082019"
+$filename = "tz_opendata_z01012019_po01112019"; 
+$sourceCSV = "$($filename).csv";
+$rowsCount = 100000;
+$rowsMaximum = 2000000;
+$startrow = 0;
+$counter = 1;
+$separateByMonthValue = "*.08.2019";
+while ($startrow -lt $rowsMaximum){
+    $ob = Import-CSV -Delimiter ';' $sourceCSV | select-object -skip $startrow -first $rowsCount 
+    foreach ($item in $ob){
+        if($item.d_reg -Like $separateByMonthValue){
+            if($item.n_reg_new -ne ""){
+                $item | Add-Member -MemberType NoteProperty -Name "PartitionKey" -Value $item.n_reg_new.Trim().Substring(0,2)
+                $item | Add-Member -MemberType NoteProperty -Name "RowKey" -Value $item.n_reg_new.Trim()
+            }
+        }
+    }
+    $ob | Export-CSV "$($filename)_$($counter).csv" -Encoding UTF8 -NoTypeInformation
+    $startrow += $rowsCount ;
+    $counter++ ;
+}
