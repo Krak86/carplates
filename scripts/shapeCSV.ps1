3a54971f3ef7254b2e7841e0c2464cdccf1a1ba7
Ruslan Korkin <rukrak86@gmail.com>
2019-11-19 10:44:31 +0200

    adjust powershell script to parse monthly car plates;

--- scripts/shapeCSV.ps1
+++ scripts/shapeCSV.ps1
@@ -1,8 +1,10 @@
 #manual step: download zip file from https://data.gov.ua/dataset/06779371-308f-42d7-895e-5a39833375f0
 #manual step: unzip it
-#manual step: put the path and file name
+#manual step: adjust the path and file name: "cd" and "$filename"
 #automatical step: add 2 new fields "PartitionKey" and "RowKey"
 #automatical step: export separate csv file per each 100000 items (it's easier to import data in azure storage explorer to azure table)
+#manual step: replace header per each file with: "person","reg_addr_koatuu","oper_code","oper_name","d_reg","dep_code","dep","brand","model","make_year","color","kind","body","purpose","fuel","capacity","own_weight","total_weight","n_reg_new","PartitionKey","RowKey"
+#manual step: import generated CSVs to azure storage explorer
 
 #path to unziped csv file
 cd "C:\Path\To\Source\csv";
--- scripts/shapeCSVbyMonth.ps1
+++ scripts/shapeCSVbyMonth.ps1
@@ -1,11 +1,13 @@
 #manual step: download zip file from https://data.gov.ua/dataset/06779371-308f-42d7-895e-5a39833375f0
 #manual step: unzip it
-#manual step: put the path and file name
+#manual step: adjust the path and file name: "cd" and "$filename"
 #automatical step: add 2 new fields "PartitionKey" and "RowKey"
 #automatical step: export separate csv file per each 100000 items (it's easier to import data in azure storage explorer to azure table)
+#manual step: replace header per each file with: "person","reg_addr_koatuu","oper_code","oper_name","d_reg","dep_code","dep","brand","model","make_year","color","kind","body","purpose","fuel","capacity","own_weight","total_weight","n_reg_new","PartitionKey","RowKey"
+#manual step: import generated CSVs to azure storage explorer
 
 #path to unziped csv file
-cd "C:\Users\rkor\Downloads\cosmosDB";
+cd "C:\Personal\rkor01\Downloads\cosmosDB";
 #sourceCSVfileName, example: "tz_opendata_z01012019_po01082019"
 $filename = "tz_opendata_z01012019_po01112019"; 
 $sourceCSV = "$($filename).csv";
@@ -15,16 +17,18 @@ $startrow = 0;
 $counter = 1;
 $separateByMonthValue = "*.08.2019";
 while ($startrow -lt $rowsMaximum){
-    $ob = Import-CSV -Delimiter ';' $sourceCSV | select-object -skip $startrow -first $rowsCount 
-    foreach ($item in $ob){
-        if($item.d_reg -Like $separateByMonthValue){
-            if($item.n_reg_new -ne ""){
-                $item | Add-Member -MemberType NoteProperty -Name "PartitionKey" -Value $item.n_reg_new.Trim().Substring(0,2)
-                $item | Add-Member -MemberType NoteProperty -Name "RowKey" -Value $item.n_reg_new.Trim()
-            }
-        }
+    $ob_source = Import-CSV -Delimiter ';' $sourceCSV | select-object -skip $startrow -first $rowsCount
+    $ob_filtered = @()
+    foreach ($item in $ob_source){
+        if(($item.d_reg -Like $separateByMonthValue) -and ($item.n_reg_new -ne "")){            
+            $item | Add-Member -MemberType NoteProperty -Name "PartitionKey" -Value $item.n_reg_new.Trim().Substring(0,2)
+            $item | Add-Member -MemberType NoteProperty -Name "RowKey" -Value $item.n_reg_new.Trim()
+            $ob_filtered = $ob_filtered + $item
+        }       
     }
-    $ob | Export-CSV "$($filename)_$($counter).csv" -Encoding UTF8 -NoTypeInformation
+    if($ob_filtered.Length -ne 0){
+        $ob_filtered | Export-CSV "$($filename)_$($counter).csv" -Encoding UTF8 -NoTypeInformation
+    }
     $startrow += $rowsCount ;
     $counter++ ;
 }
