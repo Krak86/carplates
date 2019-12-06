77d12f2b6bc1b47292a5ddb363ad19f72a73b0d5
Ruslan Korkin <rukrak86@gmail.com>
2019-12-06 12:58:57 +0200

    deploy from azure pipeline

---
+++ precache-manifest.550587087d3e63222d6ff276405a4919.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c312ccf2a724a206b062be0acda9a5a",
+    "url": "audio/sharp.mp3"
+  },
+  {
+    "revision": "563e6d30d029476ee8a3e329f6854a8e",
+    "url": "index.html"
+  },
+  {
+    "url": "js/main~52820a4b.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/main~8084949c.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/main~d0ae3f07.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/runtime.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~0436ed57.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~0928ebd2.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~0eba4586.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~0f485567.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~10e2e882.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~135a2f37.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~2a42e354.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~46e0dd6e.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~5a2dc592.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~607b9b99.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~678f84af.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~73914085.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~7cea7bec.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~7d359b94.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~8afe242f.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~98ad2b1b.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~9c5b28f6.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~bad953a5.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~cd01edf3.6ff913d22828990520c4.js"
+  },
+  {
+    "url": "js/vendors~main~f734b0c6.6ff913d22828990520c4.js"
+  },
+  {
+    "revision": "c7d675a6ee5ff4d4681b5ebed7e345de",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "0446b2c711e67fee797934f3d3b1a632",
+    "url": "policy/policy.pdf"
+  },
+  {
+    "revision": "388ed88eec82ddeacbf877ee7dc4b225",
+    "url": "robots.txt"
+  }
+]);
