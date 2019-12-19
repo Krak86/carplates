817b037789d4c06abbb6cfcc25e9379e8a246c26
krak86 <rukrak86@gmail.com>
2019-12-19 09:57:31 +0000

    Automated Release 59

---
+++ docs/precache-manifest.22805e18391734b8ab2befe9551ff5f7.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c312ccf2a724a206b062be0acda9a5a",
+    "url": "audio/sharp.mp3"
+  },
+  {
+    "revision": "ae6294a5b39b0091b1ca1a580d47782b",
+    "url": "index.html"
+  },
+  {
+    "url": "js/main.a40b71cf11e0cf02675f.js"
+  },
+  {
+    "url": "js/runtime.a40b71cf11e0cf02675f.js"
+  },
+  {
+    "url": "js/vendors~main.a40b71cf11e0cf02675f.js"
+  },
+  {
+    "revision": "c7d675a6ee5ff4d4681b5ebed7e345de",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "0446b2c711e67fee797934f3d3b1a632",
+    "url": "policy/policy.pdf"
+  },
+  {
+    "revision": "388ed88eec82ddeacbf877ee7dc4b225",
+    "url": "robots.txt"
+  }
+]);
