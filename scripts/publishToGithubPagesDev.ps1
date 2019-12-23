2af1e735de2be0f9aa4c97d636731fb70df52171
Ruslan Korkin <rukrak86@gmail.com>
2019-12-23 16:38:20 +0200

    add 'Publish to Github Pages' yml for Dev;

---
+++ scripts/publishToGithubPagesDev.ps1
@@ -0,0 +1,21 @@
+$docPath = "docs"
+$to = "ghpages"
+$githubusername = "krak86"
+$githubemail = "rukrak86@gmail.com"
+$githubaccesstoken = "$(GITHUB-PERSONAL-ACCESS-TOKEN)"
+$repositoryname = "carplates"
+$commitMessage = "Azure DevOps release " + $(Release.ReleaseId)
+$branchName = "gh-pages"
+git clone https://${githubusername}:$githubaccesstoken@github.com/$githubusername/$repositoryname.git --branch=$branchName $to --quiet
+Remove-Item $to\* -Recurse
+Copy-Item -Path $docPath\* -Destination $to -Recurse
+cd $to
+New-Item -Path . -Name "CNAME" -Force -Value @'
+carsua.pp.ua
+'@
+git config core.autocrlf false
+git config user.email $githubemail
+git config user.name $githubusername
+git add .
+git commit --allow-empty -m $commitMessage
+git push
